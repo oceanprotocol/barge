50299049aee7d50942418aaf43cd190b583a0e02
Sebastian Gerske <sebastian@oceanprotocol.com>
2018-08-10 12:34:16 +0200

    deleted old stuff

--- ocean-db/bdb-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -x
-
-curl -L https://github.com/bigchaindb/bigchaindb/archive/master.zip -o bigchaindb.zip
-#unzip bigchaindb.zip -d . && mv bigchaindb-master/* . && rm -rf bigchaindb-master bigchaindb.zip
-unzip bigchaindb.zip -d . && cd bigchaindb-master
-docker-compose build bigchaindb
-cd ..
-rm -rf bigchaindb-master bigchaindb.zip
-
