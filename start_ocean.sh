78e66c929222d9cd13d04b6aa365f9242e4b048d
ssallam <samer@oceanprotocol.com>
2018-09-27 13:15:43 +0200

    Replace unnecessary files with arguments.

--- start_ocean.sh
+++ start_ocean.sh
@@ -5,9 +5,22 @@ export OCEAN_VERSION=latest
 export DEPLOY_CONTRACTS="true"
 # Ganache specific option, these two options have no effect when not running ganache-cli
 export GANACHE_DATABASE_PATH="."
-export REUSE_DATABASE="true"
+export REUSE_DATABASE="false"
 # Specify which ethereum client to run or connect to: kovan, ganache, or ocean_poa_net_local
 export KEEPER_NETWORK_NAME="ganache"
 export ARTIFACTS_FOLDER=~/.ocean/keeper-contracts/artifacts
 
+if [ "$1" == "--no-pleuston" ]
+then
+export REUSE_DATABASE="true"
+docker-compose -f docker-compose-no-pleuston.yml up
+
+elif [ "$1" == "--local-parity-node" ]
+then
+export KEEPER_NETWORK_NAME="ocean_poa_net_local"
+docker-compose -f docker-compose-local-parity-node.yml up
+
+else
 docker-compose up
+
+fi
