f70ffb8e6b9f0f808b93df1f6c46ca4b93e56752
Matthias Kretschmann <m@kretschmann.io>
2018-10-02 13:38:55 +0200

    default to stable versions, make latest optional

--- start_ocean.sh
+++ start_ocean.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-export OCEAN_VERSION=latest
 # Must be set to true for the first run, change it to "false" to avoid migrating the smart contracts on each run.
 export DEPLOY_CONTRACTS="true"
 # Ganache specific option, these two options have no effect when not running ganache-cli
@@ -9,6 +8,14 @@ export REUSE_DATABASE="false"
 # Specify which ethereum client to run or connect to: kovan, ganache, or ocean_poa_net_local
 export KEEPER_NETWORK_NAME="ganache"
 export ARTIFACTS_FOLDER=~/.ocean/keeper-contracts/artifacts
+
+# default to `stable` versions of all components
+# unless `--latest` argument is given
+if [ "$1" == "--latest" ]; then
+    export OCEAN_VERSION=latest
+else
+    export OCEAN_VERSION=stable
+fi
 
 if [ "$1" == "--no-pleuston" ]
 then
