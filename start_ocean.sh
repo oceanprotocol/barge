565fe241c9df43b6c3de7ffb30a6fd12f3276c68
Javier Cortejoso <javier.cortejoso@gmail.com>
2018-11-29 12:32:38 +0100

    Added individual compose files

--- start_ocean.sh
+++ start_ocean.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+COMPOSE_DIR="${DIR}/compose-files"
 # Must be set to true for the first run, change it to "false" to avoid migrating the smart contracts on each run.
 export DEPLOY_CONTRACTS="true"
 # Ganache specific option, these two options have no effect when not running ganache-cli
-export GANACHE_DATABASE_PATH="."
+export GANACHE_DATABASE_PATH="${DIR}"
 export REUSE_DATABASE="false"
 # Specify which ethereum client to run or connect to: kovan, ganache, or ocean_poa_net_local
 export KEEPER_NETWORK_NAME="ganache"
-export ARTIFACTS_FOLDER=~/.ocean/keeper-contracts/artifacts
+export ARTIFACTS_FOLDER=$HOME/.ocean/keeper-contracts/artifacts
 
 # colors
 COLOR_R="\033[0;31m"    # red
@@ -30,7 +32,14 @@ show_banner
 
 # default to stable versions
 export OCEAN_VERSION=stable
-COMPOSE_FILE='docker-compose.yml'
+
+COMPOSE_FILES=""
+COMPOSE_FILES+=" -f ${COMPOSE_DIR}/network_volumes.yml"
+COMPOSE_FILES+=" -f ${COMPOSE_DIR}/mongo.yml"
+COMPOSE_FILES+=" -f ${COMPOSE_DIR}/keeper_contracts.yml"
+COMPOSE_FILES+=" -f ${COMPOSE_DIR}/pleuston.yml"
+COMPOSE_FILES+=" -f ${COMPOSE_DIR}/aquarius.yml"
+COMPOSE_FILES+=" -f ${COMPOSE_DIR}/brizo.yml"
 
 while :; do
     case $1 in
@@ -43,25 +52,26 @@ while :; do
             printf $COLOR_Y'Starting and reusing the database ...\n\n'$COLOR_RESET
             ;;
         --no-pleuston)
-            COMPOSE_FILE='docker-compose-no-pleuston.yml'
+            COMPOSE_FILES=${COMPOSE_FILES/ -f ${COMPOSE_DIR}\/pleuston.yml/}
             printf $COLOR_Y'Starting without Pleuston...\n\n'$COLOR_RESET
             ;;
         --local-parity-node)
+            COMPOSE_FILES+=" -f ${COMPOSE_DIR}/parity_client.yml"
+            COMPOSE_FILES+=" -f ${COMPOSE_DIR}/secret_store.yml"
             export KEEPER_NETWORK_NAME="ocean_poa_net_local"
-            COMPOSE_FILE='docker-compose-local-parity-node.yml'
             printf $COLOR_Y'Starting with local Parity node...\n\n'$COLOR_RESET
             ;;
         --) # End of all options.
-             shift
-             break
-             ;;
+            shift
+            break
+            ;;
         -?*)
-             printf $COLOR_R'WARN: Unknown option (ignored): %s\n'$COLOR_RESET "$1" >&2
-             break
-             ;;
+            printf $COLOR_R'WARN: Unknown option (ignored): %s\n'$COLOR_RESET "$1" >&2
+            break
+            ;;
         *)
             printf $COLOR_Y'Starting Ocean...\n\n'$COLOR_RESET
-            docker-compose --project-name=ocean -f $COMPOSE_FILE up
+            docker-compose --project-name=ocean $COMPOSE_FILES up
             break
     esac
     shift
