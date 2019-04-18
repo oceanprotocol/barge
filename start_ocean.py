99f8161f698dd50e5674d0b96e5ec706e1572080
Ulfasaar <weyers.ryan@gmail.com>
2019-04-18 22:25:03 +1200

    started cleaning code

--- start_ocean.py
+++ start_ocean.py
@@ -31,8 +31,6 @@ BRIZO_ENV_FILE = libs.export("BRIZO_ENV_FILE", DIR + "/brizo.env")
 
 # ! above not needed because its Python? need to run tests to make sure
 
-# export PROJECT_NAME="ocean"
-# export FORCEPULL="false"
 
 COMPOSE_DIR = libs.export("COMPOSE_DIR", DIR + "/compose-files")
 PROJECT_NAME = libs.export("PROJECT_NAME", "ocean")
@@ -50,11 +48,6 @@ KEEPER_VERSION = None
 PLEUSTON_VERSION = None
 
 # keeper options
-# export KEEPER_DEPLOY_CONTRACTS="false"
-# export KEEPER_ARTIFACTS_FOLDER="${HOME}/.ocean/keeper-contracts/artifacts"
-# # Specify which ethereum client to run or connect to: development, kovan, spree or nile
-# export KEEPER_NETWORK_NAME="nile"
-# export NODE_COMPOSE_FILE="${COMPOSE_DIR}/nodes/nile_node.yml"
 
 KEEPER_DEPLOY_CONTRACTS = libs.export("KEEPER_DEPLOY_CONTRACTS", "false")
 
@@ -70,57 +63,24 @@ KEEPER_VERSION = libs.export("KEEPER_VERSION", "latest")
 NODE_COMPOSE_FILE = libs.export("NODE_COMPOSE_FILE", COMPOSE_DIR + "/nodes/nile_node.yml")
 
 # Ganache specific option, these two options have no effect when not running ganache-cli
-# export GANACHE_DATABASE_PATH="${DIR}"
-# export GANACHE_REUSE_DATABASE="false"
 
 GANACHE_DATABASE_PATH = libs.export("GANACHE_DATABASE_PATH", DIR)
 GANACHE_REUSE_DATABASE = libs.export("GANACHE_REUSE_DATABASE", "false")
 
 # Specify the ethereum default RPC container provider
-# export KEEPER_RPC_HOST='keeper-node'
-# export KEEPER_RPC_PORT='8545'
-# export KEEPER_RPC_URL="http://"${KEEPER_RPC_HOST}:${KEEPER_RPC_PORT}
-# export KEEPER_MNEMONIC=''
 
 KEEPER_RPC_HOST = libs.export("KEEPER_RPC_HOST", 'keeper-node')
 KEEPER_RPC_PORT = libs.export("KEEPER_RPC_PORT", '8545')
 KEEPER_RPC_URL = libs.export("KEEPER_RPC_URL", "http://{}:{}".format(KEEPER_RPC_HOST, KEEPER_RPC_PORT))
 KEEPER_MNEMONIC = libs.export("KEEPER_MNEMONIC", '')
 
-# # Enable acl-contract validation in Secret-store
-# export CONFIGURE_ACL="true"
-# export ACL_CONTRACT_ADDRESS=""
+# Enable acl-contract validation in Secret-store
 
 CONFIGURE_ACL = libs.export("CONFIGURE_ACL", "true")
 ACL_CONTRACT_ADDRESS = libs.export("ACL_CONTRACT_ADDRESS", "")
 
-# # Export User UID and GID
-# export LOCAL_USER_ID=$(id -u)
-# export LOCAL_GROUP_ID=$(id -g)
-
-# LOCAL_USER_ID = libs.export("LOCAL_USER_ID", libs.run('id -u'))
 LOCAL_USER_ID = libs.export("LOCAL_USER_ID", libs.get_user_id())
 LOCAL_GROUP_ID = libs.export("LOCAL_GROUP_ID", libs.get_group_id())
-
-
-
-#! need to get arg 1 and store it
-
-# function get_acl_address {
-
-    #! takes param 1 if its not set sets it to -latest needs to avoid argparser or use regex
-#     local version="${1:-latest}"
-#     line=$(grep "^${version}=" "${DIR}/${KEEPER_NETWORK_NAME}_acl_contract_addresses.txt")
-#     address="${line##*=}"
-#     [ -z "${address}" ] && echo "Cannot determine the ACL Contract Address for ${KEEPER_NETWORK_NAME} version ${version}. Exiting" && exit 1
-#     echo "${address}"
-# }
-
-# function show_banner {
-#     local output=$(cat .banner)
-#     echo -e "$COLOR_B$output$COLOR_RESET"
-#     echo ""
-# }
 
 
 colorama.init()
@@ -129,8 +89,6 @@ libs.show_banner()
 
 print(COLOR_RESET)
 
-
-# DOCKER_COMPOSE_EXTRA_OPTS="${DOCKER_COMPOSE_EXTRA_OPTS:-}"
 
 # if extra ops is empty or null set it to blank
 DOCKER_COMPOSE_EXTRA_OPTS = libs.export("DOCKER_COMPOSE_EXTRA_OPTS", libs.default("DOCKER_COMPOSE_EXTRA_OPTS", ""))
@@ -217,7 +175,7 @@ if args.no_acl_contract:
 if args.local_kovan_node:
     NODE_COMPOSE_FILE = libs.export("NODE_COMPOSE_FILE", COMPOSE_DIR + "/nodes/kovan_node.yml")
     KEEPER_NETWORK_NAME = libs.export("KEEPER_NETWORK_NAME", "kovan")
-    ACL_CONTRACT_ADDRESS = libs.export("ACL_CONTRACT_ADDRESS", libs.get_acl_address(KEEPER_VERSION))
+    ACL_CONTRACT_ADDRESS = libs.export("ACL_CONTRACT_ADDRESS", libs.get_acl_address('.', 'kovan', KEEPER_VERSION))
     libs.notify("Starting with local Kovan node")
 
 if args.local_ganache_node:
@@ -234,7 +192,7 @@ if args.local_ganache_node:
 if args.local_nile_node:
     NODE_COMPOSE_FILE = libs.export("NODE_COMPOSE_FILE", COMPOSE_DIR + "/nodes/nile_node.yml")
     KEEPER_NETWORK_NAME = libs.export("KEEPER_NETWORK_NAME", "nile")
-    ACL_CONTRACT_ADDRESS = libs.export("ACL_CONTRACT_ADDRESS", libs.get_acl_address(KEEPER_VERSION))
+    ACL_CONTRACT_ADDRESS = libs.export("ACL_CONTRACT_ADDRESS", libs.get_acl_address('.', 'nile', KEEPER_VERSION))
     libs.notify("Starting with local Nile node")
 
 if args.local_spree_node:
@@ -283,8 +241,6 @@ COMPOSE_FILES = " ".join(list(map(lambda x: "-f {}/{}.yml".format(COMPOSE_DIR, x
 
 if len(NODE_COMPOSE_FILE) > 0:
     COMPOSE_FILES = COMPOSE_FILES + " -f " + NODE_COMPOSE_FILE
-    # compose_files.add(NODE_COMPOSE_FILE)
-
 
 if FORCEPULL == "true":
     libs.run("docker-compose {} --project-name={} {} pull".format(DOCKER_COMPOSE_EXTRA_OPTS, PROJECT_NAME, COMPOSE_FILES))
