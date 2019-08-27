49da094576233fe44ef0011b4f0abfd50c225e5e
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-08-27 11:50:50 +0200

    bring back secret store account

--- start_ocean.sh
+++ start_ocean.sh
@@ -282,13 +282,11 @@ while :; do
         --local-duero-node)
             export NODE_COMPOSE_FILE="${COMPOSE_DIR}/nodes/duero_node.yml"
             COMPOSE_FILES="${COMPOSE_FILES/ -f ${COMPOSE_DIR}\/keeper_contracts.yml/}"
-            COMPOSE_FILES="${COMPOSE_FILES/ -f ${COMPOSE_DIR}\/secret_store.yml/}"
             export KEEPER_MNEMONIC=''
             export KEEPER_NETWORK_NAME="duero"
             export KEEPER_DEPLOY_CONTRACTS="false"
             export ACL_CONTRACT_ADDRESS="$(get_acl_address ${KEEPER_VERSION})"
             printf $COLOR_Y'Starting with local Duero node...\n\n'$COLOR_RESET
-            printf $COLOR_Y'Starting without Secret Store...\n\n'$COLOR_RESET
             ;;
         # connects you to Pacific ocean network
         --local-pacific-node)
