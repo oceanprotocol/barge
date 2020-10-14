10e3ebeaaea558794f3ede594928e1143f8f798b
Alex Coseru <alex.coseru@gmail.com>
2020-10-14 21:09:31 +0300

    Update start_ocean.sh

--- start_ocean.sh
+++ start_ocean.sh
@@ -29,7 +29,7 @@ COMPOSE_DIR="${DIR}/compose-files"
 # Default versions of Aquarius, Provider
 
 export AQUARIUS_VERSION=${AQUARIUS_VERSION:-v3}
-export PROVIDER_VERSION=${PROVIDER_VERSION:-v0.3.0}
+export PROVIDER_VERSION=${PROVIDER_VERSION:-latest}
 export CONTRACTS_VERSION=${CONTRACTS_VERSION:-v0.5.5}
 export PROJECT_NAME="ocean"
 export FORCEPULL="false"
